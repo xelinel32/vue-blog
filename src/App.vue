33416e6d3bd3903e07285cc1b0a542a712ee70a3
xelinel32 <artemsedlyar99@gmail.com>
2020-09-27 13:46:08 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,3 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <div id="app"></div>
 </template>
